fa7d9605ad9a84df8dcd17fa86ac3c66053fbad9
Jackson Coelho <fcjack@gmail.com>
2016-08-17 15:51:19 -0300

    PermCheck

---
+++ PermCheck.java
@@ -0,0 +1,28 @@
+public class Solution {
+
+    private static final int PERMUTATION = 1;
+    private static final int NOT_PERMUTATION = 0;
+
+    private static int solution(int[] A) {
+        int[] counter = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            int value = A[i];
+            if (value < 1 || value > A.length) {
+                return NOT_PERMUTATION;
+            } else {
+                if (counter[value - 1] > 0) {
+                    return NOT_PERMUTATION;
+                } else {
+                    counter[value - 1]++;
+                }
+            }
+        }
+
+        return PERMUTATION;
+    }
+
+    public static void main(String[] args) {
+        int result = solution(new int[]{4, 3, 2, 10});
+        System.out.println(result);
+    }
+}
